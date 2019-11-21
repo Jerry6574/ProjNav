1594600513ca82d0a57e790a24da7d5c85da1ae9
jerryw <wu@assmann-wsw.com>
2019-11-21 14:18:12 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import tkinter as tk
+
+
+PROJ_DIR = r"O:\PROJECTS"
+CAB_DIR = os.path.join(PROJ_DIR, "1- CAB")
+CAB_ALT_DIR = os.path.join(CAB_DIR, "CAB finished -- CAB0001 to 1000")
+CON_DIR = os.path.join(PROJ_DIR, "2- CONN")
+HSK_DIR = os.path.join(PROJ_DIR, "3- HSK")
+ITN_DIR = os.path.join(PROJ_DIR, "6- ITN")
+
+
+
+def main():
+    return 1
+
+
+if __name__ == '__main__':
+    main()
