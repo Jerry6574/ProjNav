aa1abb6b8bb02beec41201ebf0970fb5c86f7921
nchannelmosfet <nchannelmosfet@gmail.com>
2019-12-16 19:35:55 -0700

    Added README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project Navigator
+Project Navigator, short as, ProjNav, is a simple tkinter app the opens a "Project Folder" in Windows Explorer on a "Project Number" input. 
+A "Project Number" in this scenario is a string that starts with "CAB", "CON", "HSK" or "ITN", then follows by 4 or more digits. 
+
+# Prerequisites
+For the sake of convenience, the prerequisite directories have been hard coded just for personal use. 
+This app will only work on Windows. 
+
+# Running the tests
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 from tkinter import Tk, Label, Button, Entry
 import tkinter.font as font
-import subprocess
 
-
+# configure the prerequisite directories, they are hard coded for my personal use.
 PROJ_DIR = r"O:\PROJECTS"
 CAB_DIR = os.path.join(PROJ_DIR, "1- CAB")
 CAB_ALT_DIR = [os.path.join(CAB_DIR, "CAB finished -- CAB0001 to 1000")]
@@ -20,6 +19,10 @@ ITN_ALT_DIR = None
 
 
 def get_proj_type_dirs(proj_type):
+    """
+    :param proj_type: acceptable values are "CAB", "CON", "HSK" and "ITN", raise ValueError otherwise.
+    :return: a tuple of primary_dir (primary directory) and alt_dir (alternative directory)
+    """
     if proj_type == "CAB":
         primary_dir = CAB_DIR
         alt_dir = CAB_ALT_DIR
@@ -53,12 +56,15 @@ def get_proj_dir(proj_type_dir, proj_num):
 def open_proj_dir(proj_num):
     proj_type = proj_num[0:3]
     primary_dir, alt_dirs = get_proj_type_dirs(proj_type)
-    proj_dir = get_proj_dir(primary_dir, proj_num)
 
+    # try to search project directory in the primary directory
+    proj_dir = get_proj_dir(primary_dir, proj_num)
     if proj_dir is not None:
         proj_full_dir = os.path.join(primary_dir, proj_dir)
+        # opens the project directory in Windows Explorer
         os.startfile(proj_full_dir)
 
+    # try to search project directory in the alternative directory if not found in primary directory
     elif proj_dir is None and alt_dirs is not None:
         for alt_dir in alt_dirs:
             proj_dir = get_proj_dir(alt_dir, proj_num)
@@ -66,6 +72,7 @@ def open_proj_dir(proj_num):
                 proj_full_dir = os.path.join(alt_dir, proj_dir)
                 os.startfile(proj_full_dir)
 
+        # project directory is not found in either primary or alternative directory
         if proj_dir is None:
             raise FileNotFoundError("Project folder for " + proj_num + " does not exist. ")
 
@@ -74,6 +81,7 @@ class ProjNav(Tk):
     def __init__(self):
         Tk.__init__(self)
 
+        # configure tkinter app layout
         lbl_proj_num = Label(self, text="Project Number")
         lbl_proj_num.grid(row=0)
         lbl_proj_num.config(font=("Helvetica", 20))
@@ -82,26 +90,32 @@ class ProjNav(Tk):
         self.en_proj_num.grid(row=1)
         self.en_proj_num.config(font=("Helvetica", 20))
 
-        self.btn_search = Button(self, text="Search", width=25, command=self.open_dir)
+        self.btn_search = Button(self, text="Search", width=25, command=self.open_project_dir)
         self.btn_search.grid(row=2)
         myfont = font.Font(family='Helvetica', size=20)
         self.btn_search['font'] = myfont
         self.btn_search.grid(row=2)
 
     def get_proj_num(self):
+        """
+        Retrieve the text from en_proj_num Entry box, then clear the text.
+        """
         proj_num = self.en_proj_num.get()
         self.en_proj_num.delete(0, 'end')
         return proj_num
 
-    def open_dir(self, event=None):
+    def open_project_dir(self, event=None):
         proj_num = self.get_proj_num()
         open_proj_dir(proj_num)
 
 
 def main():
     app = ProjNav()
-    app.bind('<Return>', app.open_dir)
 
+    # bind enter key to open_project_dir function
+    app.bind('<Return>', app.open_project_dir)
+
+    app.title("ProjNav")
     app.mainloop()
 
 
